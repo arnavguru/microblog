2fef48ca964f9267b92f7a2b94530ba66f853be6
arnavguru <arnav.guru@accenture.com>
2020-05-18 23:58:37 +0530

    Profile Page and Avatars

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,16 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {
-        'username': 'Arnav'
-    }
     posts = [
         {
             'author': {'username': 'John'},
@@ -40,11 +38,11 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
+        user_logging_in = User.query.filter_by(username=form.username.data).first()
+        if user_logging_in is None or not user_logging_in.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
+        login_user(user_logging_in, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
@@ -64,12 +62,52 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User()
-        user.username = form.username.data
-        user.email = form.email.data
-        user.set_password(form.password.data)
-        db.session.add(user)
+        register_user = User()
+        register_user.username = form.username.data
+        register_user.email = form.email.data
+        register_user.set_password(form.password.data)
+        db.session.add(register_user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    profile_user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {
+            'author': profile_user,
+            'body': 'Test Post #1'
+        },
+        {
+            'author': profile_user,
+            'body': 'Test Post #2'
+        }
+    ]
+    return render_template('user.html', user=profile_user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
